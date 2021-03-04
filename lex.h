1bba55536683c00c31275dee87c60d1ed3d817df
walidber <70375012+walidber@users.noreply.github.com>
2021-03-05 00:06:04 +0100

    Update lex.h

--- lex.h
+++ lex.h
@@ -1,22 +1,21 @@
 #ifndef COMPILA_LEX_H
-#define COMPILA_LEX_H
+#define COMPILA_LEX_H 1
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
 #include<stdbool.h>
+
+
 #define MAXDIGIT 11
 #define MAXCHAR 20
-#define MAXID 20
 #define NBRKEYWORDS 47
 #define NBRSPECIALTOKENS 42
 #define len 20
-#define TRUE 1
-#define FALSE 0
 
-const char* keyword[] = {"CHAR","SHORT","INT","LONG","FLOAT","DOUBLE","UNSIGNED","VOID","TRUE","FALSE","ENUM","UNION","STRING","STATIC","MALLOC","REALLOC","CALLOC","IF","ELSE","FOR","FOREACH","LOOP","BREAK","CONTINUE","GOTO","WHILE","DO","SWITCH","CASE","BOOL","FILE","CONST","VOLATILE","REGISTER","ARRAY","BYTE","RECORD","RETURN","START","END","EOF","READ","WRITE","WRITELN","OPEN","CLOSE","ECHO"};
-const char* spec_list[] = {"PV","PT","PLUS","MOINS","MULT","DIV","VIR","MOD","INC","DEC","ETBIT","OUBIT","TOGGLE","NOTBIT","DECG","DECD","INF","INFEG","EG","DIFF","SUPEG","SUP","ETLOG","OULOG","DIFF","ACCESTRU","AFF","AFFMOINS","AFFPLUS","AFFMULT","AFFDIV","AFFMOD","AFFET","AFFTOG","FINCOM","DEBCOM","ACO","ACF","PO","PF","GUI"};
-const char* spec_sym[] = {";", ".", "+", "-", "*", "/", ",", "%", "++", "--", "&", "|", "^", "~", "<<", ">>", "<", "<=", "==", "!", ">=", ">", "&&", "||", "!", "->", "<-", "-=", "+=", "*=", "/=", "%=", "&=", "^=", "?", "$", "{", "}", "(", ")","\""};
+
+
 
 typedef struct Token{
 
@@ -25,13 +24,15 @@ typedef struct Token{
 
 }Token;
 
-Token token_cour;
+Token token_cour ;
+
+
 
 char Lire_car();
 
 char ignorer_separateur(char , FILE* );
 
-char ignorer_comentaire(char, FILE*);
+char ignorer_comentaire(char , FILE* );
 
 void messageErreur();
 
@@ -39,8 +40,8 @@ char Lire_mots( char , FILE* );
 
 char Lire_nombre( char , FILE* );
 
-char Lire_special( char , FILE* );
+char Lire_special( char , FILE*);
 
-char Lire_text(char,FILE* );
+char Lire_text(char ,FILE* );
 
-#endif //COMPILA_LEX_H
+#endif
