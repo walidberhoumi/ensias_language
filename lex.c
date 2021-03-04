3bc884bccc5b67fabcb240020d039bd5bf9c24b3
walidber <70375012+walidber@users.noreply.github.com>
2021-03-05 00:05:01 +0100

    Update lex.c

--- lex.c
+++ lex.c
@@ -1,5 +1,9 @@
 #include "lex.h"
 
+const char* keyword[] = {"CHAR","SHORT","INT","LONG","FLOAT","DOUBLE","UNSIGNED","VOID","TRUE","FALSE","ENUM","UNION","STRING","STATIC","MALLOC","REALLOC","CALLOC","IF","ELSE","FOR","FOREACH","LOOP","BREAK","CONTINUE","GOTO","WHILE","DO","SWITCH","CASE","BOOL","FILE","CONST","VOLATILE","REGISTER","ARRAY","BYTE","RECORD","RETURN","START","END","EOF","READ","WRITE","WRITELN","OPEN","CLOSE","ECHO"};
+const char* spec_list[] = {"PV","PT","PLUS","MOINS","MULT","DIV","VIR","MOD","INC","DEC","ETBIT","OUBIT","TOGGLE","NOTBIT","DECG","DECD","INF","INFEG","EG","DIFF","SUPEG","SUP","ETLOG","OULOG","DIFF","ACCESTRU","AFF","AFFMOINS","AFFPLUS","AFFMULT","AFFDIV","AFFMOD","AFFET","AFFTOG","FINCOM","DEBCOM","ACO","ACF","PO","PF","GUI"};
+const char* spec_sym[] = {";", ".", "+", "-", "*", "/", ",", "++", "--", "&","%", "|", "^", "~", "<<", ">>", "<", "<=", "==", "!", ">=", ">", "&&", "||", "!", "->", "<-", "-=", "+=", "*=", "/=", "%=", "&=", "^=", "?", "$", "{", "}", "(", ")","\""};
+
 char Lire_car(FILE* fichier)
 {
     return fgetc(fichier);
@@ -43,7 +47,7 @@ void messageErreur(char* msg)
     printf("\n\n%s\n",msg);
     printf("***********************************************\n");
     printf("*************  FIN COMPILATION  ***************\n");
-    printf("***********************************************");
+    printf("***********************************************\n");
     exit(0);
 }
 
@@ -51,14 +55,14 @@ void messageErreur(char* msg)
 char Lire_mots(char car_cour,FILE* target)
 {
     char word[MAXCHAR];
-    int i=0, j=0, cmp=0;
+    int i = 0, j = 0, cmp = 0;
     word[0] = toupper(car_cour);
     while( ++i < MAXCHAR && (isalnum( word[i] = car_cour = toupper((unsigned char)Lire_car(target) )) || word[i] == '_' ));
     if( i == MAXCHAR )
         messageErreur("ERREUR IDENTIFICATEUR");
     word[i] = '\0';
-    while( (cmp=strcmp(word , keyword[j++])) != 0 && j < NBRKEYWORDS);
-    //mots_clés
+    while( (cmp = strcmp(word , keyword[j++])) != 0 && j < NBRKEYWORDS);
+
     if(cmp == 0)
     {
         strcpy(token_cour.name, word);
@@ -101,10 +105,10 @@ char Lire_special(char car_cour,FILE* target)
     }
     else
     {
-            sym[1] = car_cour = Lire_car(target);
-            if((sym[1] != '=') && (sym[1] != '<')  && (sym[1] != '>')  && (sym[1] != '&')  && (sym[1] != '|')  && (sym[1] != '+')  && (sym[1] != '-'))
+        sym[1] = car_cour = Lire_car(target);
+        if((sym[1] != '=') && (sym[1] != '<')  && (sym[1] != '>')  && (sym[1] != '&')  && (sym[1] != '|')  && (sym[1] != '+')  && (sym[1] != '-'))
             sym[1] = '\0';
-            else
+        else
             car_cour = Lire_car(target);
         while( (cmp = strcmp(sym, spec_sym[i++]) != 0) && i < NBRSPECIALTOKENS);
         if(cmp !=0)
@@ -119,24 +123,25 @@ char Lire_special(char car_cour,FILE* target)
         }
     }
 
-   
+
     return car_cour;
 }
 
 /*------------text Writeln----------*/
-char Lire_text(char car,FILE* target) 
+char Lire_text(char car_cour,FILE* target)
 {
-        car = Lire_car(target);
-        while (car != '\"' && car != EOF)
-         {
-            car = Lire_car(target);
-            if (car == '\"') {
-                strcpy(token_cour.code,"String");
-                car = Lire_car(target);
-                break;
-            }
-            if (car == EOF) {
-                messageErreur("ERREUR TEXT : Function Write");
-            }
-        }
+    car_cour = Lire_car(target);
+    while (car_cour != '\"' && car_cour != EOF)
+    {
+        car_cour = Lire_car(target);
+
+    }
+    if (car_cour == '\"') {
+        strcpy(token_cour.code,"String");
+        car_cour = Lire_car(target);
+        return car_cour;
+    }
+    else if (car_cour == EOF) {
+        messageErreur("ERREUR TEXT : Function Write");
+    }
 }
